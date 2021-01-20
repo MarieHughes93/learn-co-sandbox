e90eb05c2d88746e14b402c392dd2f912f7db7e1
Marie Hughes <contact@mariehughes.net>
2021-01-20 02:28:41 +0000

    Automatically backed up by Learn

---
+++ test.rb
@@ -0,0 +1,23 @@
+def restaurant
+  restaurant_name = "Guy's American Kitchen & Bar"
+  cuisine = "american"
+  motto = "Welcome to Flavor Town!"
+  puts motto
+end
+
+def print_and_return_name
+  puts "Guy Fieri"
+  "Guy Fieri"
+end
+
+def broken_print_and_return_name
+  "Guy Fieri"
+  puts "Guy Fieri"
+end
+
+
+def i_made_this_stuff(arg ='dork')
+  puts "Whatever the hell I want #{arg}!"
+end
+
+i_made_this_stuff
